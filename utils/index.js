9063ece0e37c612f9a236562f454d93027d7e4b8
qNanna <evg2287@gmail.com>
2021-08-20 13:25:52 +0300

    fix: getDependencies

--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,19 @@
-// TODO: HASH
 import crypto from 'crypto'
+import fs from 'fs/promises'
 
 function getHash (data, encryption = 'sha256', encoding = 'base64') {
   return crypto.createHash(encryption).update(JSON.stringify(data)).digest(encoding)
 }
 
+async function readFile (path) {
+  try {
+    const buf = await fs.readFile(path)
+    return JSON.parse(buf.toString())
+  } catch {
+    return null
+  }
+}
 export default {
-  getHash
+  getHash,
+  readFile
 }

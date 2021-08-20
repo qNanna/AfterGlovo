9c088cac76f92ad8513bc75753ed9361efada524
qNanna <evg2287@gmail.com>
2021-08-20 15:45:06 +0300

    fix: expect only { from: string, to: string }

--- api/routers/apiConfigRouter.js
+++ api/routers/apiConfigRouter.js
@@ -5,6 +5,5 @@ import apiConfigController from '../controllers/apiConfigController.js'
 const apiConfigRouter = express.Router()
 
 apiConfigRouter.get('/getDependencies', (req, res) => apiConfigController.getDependencies(req, res))
-// apiConfigRouter.get('/setVariables', (req, res) => res.json({ status: 'Done' }))
 
 export default apiConfigRouter

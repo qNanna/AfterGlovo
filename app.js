2bb913488ba3bf66ec500ff7b4caab9e7f788d3a
Nanna <evg2287@gmail.com>
2021-08-23 11:34:57 +0300

    chor: fixed syntax app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-import config from './config/index.js'
-import chalk from 'chalk'
-import express from 'express'
+import chalk from 'chalk';
+import express from 'express';
+import config from './config/index';
 
-import api from './api/index.js'
+import api from './api/index';
 
-const app = express()
+const app = express();
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use('/api', api)
+app.use('/api', api);
 
-app.listen(config.port, config.host, () => console.info(chalk.green(`Server has started on ${config.host}:${config.port}`)))
+app.listen(config.port, config.host, () => console.info(chalk.green(`Server has started on ${config.host}:${config.port}`)));

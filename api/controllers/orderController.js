6e435677edfb508e0a786d9699cc7ca79f76dba8
Nanna <evg2287@gmail.com>
2021-08-23 16:34:24 +0300

    fix: fixed crash when adress == null

--- api/controllers/orderController.js
+++ api/controllers/orderController.js
@@ -6,24 +6,30 @@ import locationService from '../../services/locationLqService';
 
 class OrderController {
   async oneWay(req, res) {
-    res.json(this.result = 'not yet');
+    res.json(this.message = 'not yet');
   }
 
   async estimate(req, res) {
     this.key = utils.getHash(req.body);
     let data = await redis.get(this.key);
+
     if (!data) {
       const locationArr = Object.entries(req.body).map(async ([key, el]) => {
         if (key === 'from' || key === 'to') {
-          const locations = await locationService.getLocation(el);
-          el = { lat: locations.lat, lon: locations.lon };
+          const location = await locationService.getLocation(el);
+          if (!location) return {};
+          el = { lat: location.lat, lon: location.lon };
         }
         return [key, el];
       });
-      const location = Object.fromEntries(await Promise.all(locationArr));
-      const order = await glovoService.estimateOrder(location.from, location.to);
-      data = glovoService.getDiscount(order);
-      await redis.setEx(this.key, config.redisDataLifeTime, data);
+
+      const locations = Object.fromEntries(await Promise.all(locationArr));
+      const order = await glovoService.estimateOrder(locations.from, locations.to);
+      if (order && !order.error) {
+        data = await glovoService.getDiscount(order);
+        console.log(data);
+        await redis.setEx(this.key, config.redisDataLifeTime, data);
+      } else data = order || 'Something wrong';
     }
     res.json({ data });
   }
